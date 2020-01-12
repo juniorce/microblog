90657b58268ce5a1afa365b3c05d57d82e695044
Francisco das Chagas Silva Junior <francisco.junior@hbsis.com.br>
2020-01-12 13:54:37 -0300

    update

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'quando-o-sol-bater'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -14,3 +15,4 @@ class Config(object):
     ADMINS = ['compjrce@gmail.com']
     POSTS_PER_PAGE = 5
     LANGUAGES = ['en', 'es']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
